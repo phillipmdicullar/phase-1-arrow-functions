b5511442e93b67086051685168cd084f9a32764b
Philip <phillip.emdokolo@student.moringaschool.com>
2024-03-26 16:42:27 +0300

    learnt a bit of functions

---
+++ index.js
@@ -0,0 +1,9 @@
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(20,5);
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2,3);
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100);
+const square = (parameter) => parameter * parameter;
+const add = (parameter, parameter2) => parameter + parameter2;
+add(5, 3)
